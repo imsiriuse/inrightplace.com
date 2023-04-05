7091f58cd14ef923e7713b97133ada6d82559459
imsiriuse <imsiriuse@gmail.com>
2023-04-05 13:02:41 +0200

    removed application crossroads and added food instead, added basic homepage

--- inrightplace/settings.py
+++ inrightplace/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crossroads',
+    'food',
+    'django_bootstrap5'
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'inrightplace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'food', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
