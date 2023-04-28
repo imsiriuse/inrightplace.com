c7662d4ca8e43117da4191a2a6485c78152dc3d4
imsiriuse <imsiriuse@gmail.com>
2023-04-28 10:43:59 +0200

    modified models.py to make database ready for loading test data

--- food/models.py
+++ food/models.py
@@ -1,46 +1,25 @@
 from django.db import models
 
-
-class IngredientCategory(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
 class Ingredient(models.Model):
-    name = models.CharField(max_length=100)
-    category = models.ForeignKey(IngredientCategory, on_delete=models.CASCADE)
+    text = models.CharField(max_length=100)
     def __str__(self):
-        return self.name
+        return self.text
 
-class IngredientAttribute(models.Model):
-    name = models.CharField(max_length=100)
+class Step(models.Model):
+    text = models.CharField(max_length=100)
     def __str__(self):
-        return self.name
+        return self.text
 
-
-class Unit(models.Model):
-    name = models.CharField(max_length=100)
+class Content(models.Model):
+    text = models.CharField(max_length=100)
     def __str__(self):
-        return self.name
-
-class RecipeIngredient(models.Model):
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    quantity = models.CharField(max_length=50)
-    attribute = models.ForeignKey(IngredientAttribute, on_delete=models.CASCADE, null=True, blank=True)
-    unit = models.ForeignKey(Unit, on_delete=models.CASCADE, null=True, blank=True)
-    def __str__(self):
-        return f"{self.quantity} {self.ingredient.name}"
+        return self.text
 
 class Recipe(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
-    prep_time = models.PositiveIntegerField()
-    cook_time = models.PositiveIntegerField()
-    servings = models.PositiveIntegerField()
-    directions = models.TextField()
-    ingredients = models.ForeignKey(RecipeIngredient, on_delete=models.CASCADE)
+    ingredients = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    contents = models.ForeignKey(Content, on_delete=models.CASCADE)
+    steps = models.ForeignKey(Step, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
